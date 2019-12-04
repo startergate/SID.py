22e75920143d677e6ce04219be35321fcf724edd
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-04 20:05:49 +0900

    [CREATE] login function

--- app.py
+++ app.py
@@ -2,6 +2,28 @@ import requests
 
 
 class SID:
-    def __init__(self, clientName):
-        self.clientName = clientName
+    baseURL = "http://sid.donote.co:3000/api/v1/"
 
+    def __init__(self, client_name):
+        self.client_name = client_name
+
+    def login(self, clientid, id, pw, is_web=False):
+        res = requests.post(self.baseURL + '/session/' , {
+            "type": "login",
+            "clientid": clientid,
+            "userid": id,
+            "password": pw,
+            "isPermanent": False,
+            "isWeb": is_web
+        }).json()
+
+        if res["type"] == "error":
+            raise Exception("Input Data Error")
+        return {
+            "sessid": res["response_data"][0],
+            "pid": res["response_data"][1],
+            "nickname": res["response_data"][2],
+            "expire": res["response_data"][3],
+        }
+
+    
